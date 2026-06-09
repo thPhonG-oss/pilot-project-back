cff74f4d2e2442710b461746adc145cf4badb56e
TPHG <phong.nguyenthanh@elca.vn>
2026-06-09 15:03:49 +0700

    exercise: complete exercise 17, 18

--- src/main/java/vn/elca/training/service/impl/UserServiceImpl.java
+++ src/main/java/vn/elca/training/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package vn.elca.training.service.impl;
 
 import org.hibernate.Hibernate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +12,8 @@ import vn.elca.training.repository.TaskRepository;
 import vn.elca.training.repository.UserRepository;
 import vn.elca.training.service.UserService;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 /**
@@ -20,19 +24,23 @@ import java.util.List;
 @Transactional
 public class UserServiceImpl implements UserService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     UserRepository userRepository;
     @Autowired
     TaskRepository taskRepository;
 
+    @PersistenceContext
+    EntityManager em;
+
     @Override
     public User findOne(Long id) {
         User user = userRepository.findById(id).orElse(null);
         if (user != null) {
+            log.info("State: {}", em.contains(user));
             Hibernate.initialize(user.getTasks());
         }
         // Should throw exception if not found
-
         return user;
     }
 
@@ -46,6 +54,8 @@ public class UserServiceImpl implements UserService {
         List<Task> tasks = taskRepository.findAllById(taskIds);
         User user = findOne(username);
         user.setTasks(tasks);
+
+        tasks.forEach(task -> task.setUser(user));
 
         return user;
     }
